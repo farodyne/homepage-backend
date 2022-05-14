0916161defa5d5114c17c6bbedcc5c25f4970e59
Federico Engler <github@farodyne.com>
2022-05-14 14:30:24 +0200

    Added backend host information for local development mode.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/**
+ * Author: Federico Engler
+ *
+ * The entry point for my simple backend kicks off the REST API by creating an
+ * instance of the database client and injecting it to an instance of the API class.
+ * This dependency injection makes the code easier to test, but also creates proper
+ * layers for separation of concerns.
+ */
 import { RestApi } from './rest';
 import { DbClient, settings } from './utils';
 
